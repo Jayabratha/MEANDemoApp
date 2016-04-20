a37821e92dbb5b7f4b3f8a1ec96ed97da7db6107
Jayabratha <medha@Users-Mac-mini-2.local>
2016-04-20 13:12:59 +0530

    Fixed New For mobile

---
+++ public/scripts/directives/fix_to_top_directive.js
@@ -0,0 +1,18 @@
+app.directive('fixToTop', function($window) {
+  var $win = angular.element($window); // wrap window object as jQuery object
+
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var topClass = attrs.fixToTop; // get CSS class from directive's attribute value
+
+      $win.on('scroll', function(e) {
+        if ($win.scrollTop() >= 248) {
+          element.addClass(topClass);
+        } else {
+          element.removeClass(topClass);
+        }
+      });
+    }
+  };
+});
